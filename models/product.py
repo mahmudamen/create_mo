a70c83334d1f1acc77c539871d0693b8110eda45
dev1-pcp <mahmudamen@gmail.com>
2022-08-02 20:58:21 +0200

    button finish 2

--- models/product.py
+++ models/product.py
@@ -1,7 +1,6 @@
 
+from odoo.addons import decimal_precision as dp
 from datetime import datetime, timedelta
-
-
 from odoo import api, fields, models, _, SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.tools import float_compare, float_round
@@ -98,7 +97,7 @@ class MoWorkOrder(models.Model):
             self._create_or_update_finished_line()
 
         # Update workorder quantity produced
-        #self.qty_produced += self.qty_producing
+        self.qty_produced += self.qty_producing
 
         # Suggest a finished lot on the next workorder
         if self.next_work_order_id and self.product_tracking != 'none' and (not self.next_work_order_id.finished_lot_id or self.next_work_order_id.finished_lot_id == self.finished_lot_id):
