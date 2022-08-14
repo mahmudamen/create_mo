af9505000d7cd9bc91a768bcbeeb0c64a857ed84
dev1-pcp <mahmudamen@gmail.com>
2022-08-14 19:30:26 +0200

    cut number

--- models/product.py
+++ models/product.py
@@ -1,33 +1,27 @@
-
 from odoo.addons import decimal_precision as dp
 from datetime import datetime, timedelta
 from odoo import api, fields, models, _, SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.tools import float_compare, float_round
 
+
 class product(models.Model):
     _inherit = 'product.product'
     _description = 'Description'
 
-    product_name = fields.Char(string='name',related="product_tmpl_id.name")
+    product_name = fields.Char(string='name', related="product_tmpl_id.name")
     product_source = fields.Char(string='Cut number')
     product_qty = fields.Float(string='Quantity',
                                digits=dp.get_precision(
                                    'Product Unit of Measure'))
 
-
-
-
-
-
-
     def action_create_mrp(self):
         last_id = self.env['mrp.routing'].search([])[-1].id
         for i in self:
             if i.product_qty <= 0:
-                raise UserError(_('qty cant be zero' ))
+                raise UserError(_('qty cant be zero'))
             else:
-                mrp_bom = self.env['mrp.bom'].search([('product_tmpl_id','=',i.product_tmpl_id.id)])
+                mrp_bom = self.env['mrp.bom'].search([('product_tmpl_id', '=', i.product_tmpl_id.id)])
 
                 if mrp_bom:
                     production_vals = {
@@ -36,17 +30,17 @@ class product(models.Model):
                         'product_qty': i.product_qty,
                         'date_planned_start': datetime.now() + timedelta(days=14),
                         'date_planned_finished': datetime.now() + timedelta(days=24),
-                        'product_uom_id':i.uom_id.id,
+                        'product_uom_id': i.uom_id.id,
                         # 'purchase_order_line_id': purchase_order_line_id,
-                        'origin':i.product_source,
+                        'origin': i.product_source,
                         'all_number': 1,
                         'number': 1,
                         # 'spec_url': line.spec_url,
                         # 'attachment_id': line.attachment_id.id,
                     }
                     s = self.env['mrp.production'].create(production_vals)
-                    o = self.env['mrp.production'].search([('id','=',s.id)])
-                    x = self.env['stock.picking'].search([('id','=',o.picking_ids.ids)])
+                    o = self.env['mrp.production'].search([('id', '=', s.id)])
+                    x = self.env['stock.picking'].search([('id', '=', o.picking_ids.ids)])
                     for i in x:
                         x.origin = o.origin
                     m = o._onchange_move_raw()
@@ -55,47 +49,59 @@ class product(models.Model):
                     raise UserError(_('can not find bom'))
 
 
-
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-
     previous_order_qty = fields.Float(string='Previous Order Qty', default=0.0,
-                                          compute='_compute_previous_order_qty')
+                                      compute='_compute_previous_order_qty')
+    origin = fields.Char('cut numer')
 
     def _compute_previous_order_qty(self):
         for rec in self:
             last_step_qty = 0.0
-            previous_wo = self.env['mrp.workorder'].search([ ('next_work_order_id', '=', rec.id)])
+            previous_wo = self.env['mrp.workorder'].search([('next_work_order_id', '=', rec.id)])
             last_step_qty = previous_wo.qty_produced
             rec.previous_order_qty = last_step_qty
+
+
+
 
 class MrpProduction(models.Model):
     """ Manufacturing Orders """
     _inherit = 'mrp.production'
-    def _get_finished_move_value(self, product_id, product_uom_qty, product_uom, operation_id=False, byproduct_id=False):
+
+    def button_plan(self):
+        res = super(MrpProduction, self).button_plan()
+
+        for i in self.workorder_ids:
+            i.origin = self.origin
+
+        return res
+
+    def _get_finished_move_value(self, product_id, product_uom_qty, product_uom, operation_id=False,
+                                 byproduct_id=False):
         return {
-                'product_id': product_id,
-                'product_uom_qty': product_uom_qty,
-                'product_uom': product_uom,
-                'operation_id': operation_id,
-                'byproduct_id': byproduct_id,
-                'unit_factor': product_uom_qty / self.product_qty,
-                'name': self.name,
-                'date': self.date_planned_start,
-                'date_expected': self.date_planned_finished,
-                'picking_type_id': self.picking_type_id.id,
-                'location_id': self.product_id.with_context(force_company=self.company_id.id).property_stock_production.id,
-                'location_dest_id': self.location_dest_id.id,
-                'company_id': self.company_id.id,
-                'production_id': self.id,
-                'warehouse_id': self.location_dest_id.get_warehouse().id,
-                'origin': self.origin,
-                'group_id': self.procurement_group_id.id,
-                'propagate_cancel': self.propagate_cancel,
-                'propagate_date': self.propagate_date,
-                'propagate_date_minimum_delta': self.propagate_date_minimum_delta,
-                'move_dest_ids': [(4, x.id) for x in self.move_dest_ids if not byproduct_id],
+            'product_id': product_id,
+            'product_uom_qty': product_uom_qty,
+            'product_uom': product_uom,
+            'operation_id': operation_id,
+            'byproduct_id': byproduct_id,
+            'unit_factor': product_uom_qty / self.product_qty,
+            'name': self.name,
+            'date': self.date_planned_start,
+            'date_expected': self.date_planned_finished,
+            'picking_type_id': self.picking_type_id.id,
+            'location_id': self.product_id.with_context(force_company=self.company_id.id).property_stock_production.id,
+            'location_dest_id': self.location_dest_id.id,
+            'company_id': self.company_id.id,
+            'production_id': self.id,
+            'warehouse_id': self.location_dest_id.get_warehouse().id,
+            'origin': self.origin,
+            'group_id': self.procurement_group_id.id,
+            'propagate_cancel': self.propagate_cancel,
+            'propagate_date': self.propagate_date,
+            'propagate_date_minimum_delta': self.propagate_date_minimum_delta,
+            'move_dest_ids': [(4, x.id) for x in self.move_dest_ids if not byproduct_id],
         }
 
     def _get_move_raw_values(self, bom_line, line_data):
@@ -115,7 +121,8 @@ class MrpProduction(models.Model):
             'product_uom_qty': quantity,
             'product_uom': bom_line.product_uom_id.id,
             'location_id': source_location.id,
-            'location_dest_id': self.product_id.with_context(force_company=self.company_id.id).property_stock_production.id,
+            'location_dest_id': self.product_id.with_context(
+                force_company=self.company_id.id).property_stock_production.id,
             'raw_material_production_id': self.id,
             'company_id': self.company_id.id,
             'operation_id': bom_line.operation_id.id or alt_op,
